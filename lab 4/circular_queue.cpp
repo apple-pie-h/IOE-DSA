47308dfdbdd984bd46c6e608eb7cf24ce0f45b6c
rebika <rebikaparajuli@gmail.com>
2026-06-30 10:51:20 +0545

    Circular queue lab 4

--- lab 4/circular_queue.cpp
+++ lab 4/circular_queue.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class CircularQueue {
-public:
-    int front;
-    int rear;
-    int *queue;
-    int size;
+int queue[5];
+int front = 0;
+int rear = 0;
+int size = 5;
 
-    CircularQueue(int s) {
-        size = s;
-        queue = new int[size];
-        front = 0;
-        rear = 0;
+void enqueue(int x) {
+    if (front == (rear + 1) % size)
+        cout << "Queue is Full\n";
+    else {
+        queue[rear] = x;
+        rear = (rear + 1) % size;
     }
+}
 
-    void enqueue(int x) {
-        if (front == (rear + 1) % size)
-            cout << "Queue is full" << endl;
-        else {
-            queue[rear] = x;
-            rear = (rear + 1) % size;
-        }
-    }
-
-    void dequeue() {
-        if (front == rear)
-            cout << "Queue is empty" << endl;
-        else
-            front = (front + 1) % size;
-    }
-};
+void dequeue() {
+    if (front == rear)
+        cout << "Queue is Empty\n";
+    else
+        front = (front + 1) % size;
+}
 
 int main() {
-    CircularQueue q(5);
-
-    q.enqueue(1);
-    q.enqueue(2);
-    q.dequeue();
-    q.enqueue(3);
-    q.dequeue();
-    q.dequeue();
+    enqueue(1);
+    enqueue(2);
+    dequeue();
+    enqueue(3);
+    dequeue();
+    dequeue();
 
     return 0;
 }
